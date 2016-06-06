46ffc537cea814b21f3d68c62f17967bcea6767b
Rich Churcher <rich.churcher@gmail.com>
2016-06-06 13:33:59 +1200

    Scaffold

---
+++ classes.js
@@ -0,0 +1,23 @@
+// Don't change or delete this line! It waits until the DOM has loaded, then calls 
+// the start function. More info: 
+// https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+document.addEventListener('DOMContentLoaded', start)
+
+function start () {
+  // The first example is done for you. Uncomment the line below and reload the browser.
+  one()
+    
+  // Your turn! Create a new function called `two`, then call it from here.
+}
+
+function one () {
+  // First, we have to find the element:
+  var one = document.getElementById('one')
+
+  // Next, we apply a new CSS class to it:
+  one.classList = 'blue'
+}
+
+// CREATE FUNCTION two HERE
+
+// CREATE FUNCTION three HERE
