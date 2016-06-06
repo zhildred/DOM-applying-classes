a36e121255e45b09adb622f563975652e00a6a85
Rich Churcher <rich.churcher@gmail.com>
2016-06-06 14:24:16 +1200

    Adjust for assignment content

--- classes.js
+++ classes.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  one()
+  // one()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -21,3 +21,5 @@ function one () {
 // CREATE FUNCTION two HERE
 
 // CREATE FUNCTION three HERE
+
+// CREATE FUNCTION makeVisible HERE
